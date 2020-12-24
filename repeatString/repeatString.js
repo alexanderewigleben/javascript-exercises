580c7f803f3e14d981fe340f62d18d0d5785ddae
Alex Wolf <alexander.ewigleben@gmail.com>
2020-12-24 03:35:40 -0500

    passed all tests

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, n)
+ {  
+     if (n<0) {
+         return 'ERROR';
+     }
+    var storedString=[]
+        for (i=0; i<n; i++){
+            storedString.push(word)
+        }
+        return storedString.join("");
 
 }
 
