5b4026a53e3bc937d3c3cefca33c3b35be4953bf
Alex Wolf <alexander.ewigleben@gmail.com>
2020-12-24 04:15:18 -0500

    reversed string

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(wordToBeReversed) {
+    
+    var forwards = wordToBeReversed.split("");
+    for (i=0; i < forwards.length - 1; i++){
+        letterMoving = forwards.pop();
+        forwards.unshift(letterMoving);
+    }
+    return forwards.join("");
 }
 
 module.exports = reverseString
+
