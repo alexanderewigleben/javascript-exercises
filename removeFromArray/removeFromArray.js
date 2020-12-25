169ce79a7e0ba2da08371ac734a5ee1f118fa886
Alex Wolf <alexander.ewigleben@gmail.com>
2020-12-24 19:02:41 -0500

    remove finished

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...thingsToRemove){
 
+    const array = thingsToRemove[0];
+    const newArray=[];
+    array.forEach((item)=>{
+        if (!thingsToRemove.includes(item)){
+            newArray.push(item);
+        }
+    });
+    return newArray;
 }
+
 
 module.exports = removeFromArray
