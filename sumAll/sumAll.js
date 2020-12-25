4c8551b1914f88d59ea79b424d4ed358792debce
Alex Wolf <alexander.ewigleben@gmail.com>
2020-12-24 20:45:04 -0500

    whateverdog

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
 
-}
+        let totalSum = 0;
+      for (firstNum; firstNum < secondNum+1; firstNum++){
+          totalSum = totalSum + firstNum;
+          }
+          return totalSum;
+      }
+
+
+
 
 module.exports = sumAll
+
+
+// create a variable the sums
+// go through each number adding it to sum variable
+// incriment one
+// repeat till you read second num
+// return variable of sums
